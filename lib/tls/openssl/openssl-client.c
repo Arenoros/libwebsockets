d035478df84fbea50aaa8fed156e223268a213d3
Arenoros <arenoros@gmail.com>
2020-05-28 01:32:45 +0300

    add check strnlen

--- lib/tls/openssl/openssl-client.c
+++ lib/tls/openssl/openssl-client.c
@@ -30,6 +30,9 @@
  * Care: many openssl apis return 1 for success.  These are translated to the
  * lws convention of 0 for success.
  */
+#ifndef LWS_HAVE_STRNLEN
+#define strnlen(Str, Size) (strlen((Str)))
+#endif
 
 int lws_openssl_describe_cipher(struct lws *wsi);
 
